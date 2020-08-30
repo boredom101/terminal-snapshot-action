78c97fe271ae8a49749967d96d478e60eff7a0ec
boredom101 <ymnewmark@gmail.com>
2020-08-29 21:09:01 -0400

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -l
+
+cd /
+tar -zxf /mesa.tar.gz
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mesa/local/lib/x86_64-linux-gnu
+export GALLIUM_DRIVER=swr
+
+cd $GITHUB_WORKSPACE
+mkdir ../xvfb
+
+ROW=$(( 24*$1 ))
+COL=$(( 24*$2 ))
+
+Xvfb :99 -fbdir ../xvfb -screen 0 ${1}x${2}x8 &
+export DISPLAY=:99
+
+chmod +x $5
+$5 &
+
+sleep $4
+
+convert xwd:../xvfb/Xvfb_screen0 png:$3
